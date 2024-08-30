4eedeb995b3af201bbc39a2f932ba37cd444185a
griseldaalegre <alegregriselda5@gmail.com>
2024-08-30 15:58:29 -0300

    agrego ejercicio 2 tp4

--- src/tp4/Main.java
+++ src/tp4/Main.java
@@ -2,8 +2,18 @@ package tp4;
 public class Main {
     public static void main(String[] args) {
         //EJERCICIO 1
-        /*E1JAlarmaLuminosa alarma = new E1JAlarmaLuminosa();
+        /*1 - Alarma
+        Implementar en Java todo el código necesario para el funcionamiento de una alarma. La
+        alarma tiene tres variables que indican si: se rompió un vidrio, se abrió una puerta o ventana,
+        se detectó un movimiento dentro del domicilio. Cuando se invoca al método comprobar() de
+        la alarma, la misma chequea si alguna de sus variables indica que debe hacer sonar la señal
+        sonora.
+        Crea una clase denominada AlarmaLuminosa que, además de activar la señal sonora,
+        encienda una luz cuando alguno de los indicadores está activado.
+        Nota: Asumir que hay una clase Timbre, con un método hacerSonar() y una clase Luz con el
+        método encender().*/
 
+        /*E1JAlarmaLuminosa alarma = new E1JAlarmaLuminosa();
         alarma.setVidrioRoto(true);
         System.out.println(alarma.comprobar());  // Debería hacer sonar el timbre y encender la luz*/
 
