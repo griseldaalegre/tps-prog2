14fb983ca36b8579aed5cbfc492d741af5b2ab9d
griseldaalegre <alegregriselda5@gmail.com>
2024-08-30 16:01:37 -0300

    agrego ejercicio 2 tp4

--- src/tp4/E1JAlarmaLuminosa.java
+++ src/tp4/E1JAlarmaLuminosa.java
@@ -1,18 +1,16 @@
 package tp4;
 
-import java.util.ArrayList;
-
 public class E1JAlarmaLuminosa extends EJ1Alarma {
     private EJ1Luz luz;
 
     public E1JAlarmaLuminosa() {
-        //recordar: los constructores no se heredan.
+        //los constructores no se heredan.
         super();//llamo al contructor de mi padre
         this.luz = new EJ1Luz();
     }
 
     @Override
     public String comprobar() {
-        return super.comprobar(); // Comprueba y obtiene las zonas activas
+        return super.comprobar();
     }
 }
